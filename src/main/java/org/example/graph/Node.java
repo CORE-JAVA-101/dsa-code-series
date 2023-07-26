44860031ac082a0d76b6279b5b279e48a05f4d4f
rkpatra201 <rabindrapatra825@gmail.com>
2023-07-26 13:08:01 +0530

    graph traversal

--- src/main/java/org/example/graph/Node.java
+++ src/main/java/org/example/graph/Node.java
@@ -6,11 +6,17 @@ import java.util.List;
 public class Node {
 
   String name;
+  int weight;
   List<Node> edges;
 
   public Node(String name) {
-    this.edges = new ArrayList<>();
+    this(name, 0);
+  }
+
+  public Node(String name, int weight){
     this.name = name;
+    this.weight = weight;
+    this.edges = new ArrayList<>();
   }
 
   public String getName() {
@@ -19,6 +25,14 @@ public class Node {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public int getWeight() {
+    return weight;
+  }
+
+  public void setWeight(int weight) {
+    this.weight = weight;
   }
 
   public List<Node> getEdges() {
