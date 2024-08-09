12db45f3c35ad92560db2f9a20cd7ae29fa14c92
rkpatra201 <rabindrapatra825@gmail.com>
2024-08-09 16:52:19 +0530

    lcs changes

--- src/main/java/org/example/dp/str/LongestCommonSubsequence.java
+++ src/main/java/org/example/dp/str/LongestCommonSubsequence.java
@@ -1,32 +1,34 @@
 package org.example.dp.str;
 
-public class LongestCommonSubstring {
+public class LongestCommonSubsequence {
 
     public int solution(String p1, String p2) {
 
         int commonLength = 0;
-        commonLength = solution(p1, p2, p1.length(), p2.length(), 0);
+        commonLength = solution(p1, p2, p1.length(), p2.length());
         return commonLength;
     }
 
-    private int solution(String p1, String p2, int len1, int len2, int maxLength) {
+    private int solution(String p1, String p2, int len1, int len2) {
+
         if (len1 == 0 || len2 == 0) {
-            return maxLength;
+            return 0;
         }
+
         // when match occurs reduce each string and increment maxLength by 1
         if (p1.charAt(len1 - 1) == p2.charAt(len2 - 1)) {
-            maxLength = solution(p1, p2, len1 - 1, len2 - 1, maxLength + 1);
+            return solution(p1, p2, len1 - 1, len2 - 1) + 1;
         }
         // as no match occurred, next recursive calls will start with maxLength = 0
 
         // considering first string but reducing second string
-        int inclusionLength = solution(p1, p2, len1, len2 - 1, 0);
+        int inclusionLength = solution(p1, p2, len1, len2 - 1);
         // considering first string but reducing second string
-        int exclusionLength = solution(p1, p2, len1 - 1, len2, 0);
+        int exclusionLength = solution(p1, p2, len1 - 1, len2);
 
         int result = Math.max(inclusionLength, exclusionLength);
-        maxLength = Math.max(result, maxLength);
-        return maxLength;
+
+        return result;
 
     }
 }
